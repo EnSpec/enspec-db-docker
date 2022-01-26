a3ec6e31988f45f483a2890caed64c3c5352dad7
Justin Merz <jrmerz@gmail.com>
2022-01-26 08:25:27 -0800

    changes for flight line session relation

--- schema/tables/flightlines.sql
+++ schema/tables/flightlines.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS flightlines CASCADE;
 CREATE TABLE flightlines (
   flightlines_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   source_id UUID REFERENCES source NOT NULL,
+  session_id UUID REFERENCES source NOT NULL,
   start_time TIME NOT NULL,
   end_time TIME NOT NULL,
   media_files TEXT NOT NULL,
@@ -16,22 +17,28 @@ CREATE INDEX flightlines_source_id_idx ON flightlines(source_id);
 -- VIEW
 CREATE OR REPLACE VIEW flightlines_view AS
   SELECT
-    f.flightlines_id AS flightlines_id,
-    f.start_time  as start_time,
-    f.end_time  as end_time,
-    f.media_files  as media_files,
-    f.line_notes  as line_notes,
-    f.line_number as line_number,
-    f.line_index as line_index,
+    fl.flightlines_id AS flightlines_id,
+    f.date AS flight_date,
+    s.name AS session_name,
+    fl.start_time  as start_time,
+    fl.end_time  as end_time,
+    fl.media_files  as media_files,
+    fl.line_notes  as line_notes,
+    fl.line_number as line_number,
+    fl.line_index as line_index,
 
     sc.name AS source_name
   FROM
-    flightlines f
-LEFT JOIN source sc ON f.source_id = sc.source_id;
+    flightlines fl
+LEFT JOIN sessions s ON fl.session_id = s.session_id
+LEFT JOIN flight f ON s.session_id = f.flight_id
+LEFT JOIN source sc ON fl.source_id = sc.source_id;
 
 -- FUNCTIONS
 CREATE OR REPLACE FUNCTION insert_flightlines (
   flightlines_id UUID,
+  flight_date DATE,
+  sessions_name TEXT,
   start_time TIME,
   end_time TIME,
   media_files TEXT,
@@ -42,11 +49,14 @@ CREATE OR REPLACE FUNCTION insert_flightlines (
   source_name TEXT) RETURNS void AS $$
 DECLARE
   source_id UUID;
+  session_id UUID;
 BEGIN
 
   IF( flightlines_id IS NULL ) THEN
     SELECT uuid_generate_v4() INTO flightlines_id;
   END IF;
+  select get_session_id(flight_name, session_name) into session_id;
+
   SELECT get_source_id(source_name) INTO source_id;
 
   INSERT INTO flightlines (
@@ -59,6 +69,41 @@ EXCEPTION WHEN raise_exception THEN
   RAISE;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+-- FUNCTIONS
+CREATE OR REPLACE FUNCTION epi_insert_flightlines (
+  flightlines_id UUID,
+  session_id TEXT,
+  start_time TIME,
+  end_time TIME,
+  media_files TEXT,
+  line_notes TEXT,
+  line_number FLOAT,
+  line_index FLOAT,
+  source_name TEXT) RETURNS void AS $$
+DECLARE
+  source_id UUID;
+BEGIN
+
+  IF( flightlines_id IS NULL ) THEN
+    SELECT uuid_generate_v4() INTO flightlines_id;
+  END IF;
+
+  SELECT get_source_id(source_name) INTO source_id;
+
+  INSERT INTO flightlines (
+    flightlines_id, session_id, start_time, end_time, media_files, line_notes, line_number, line_index, source_id
+  ) VALUES (
+    flightlines_id, session_id, start_time, end_time, media_files, line_notes, line_number, line_index, source_id
+  );
+
+EXCEPTION WHEN raise_exception THEN
+  RAISE;
+END;
+$$ LANGUAGE plpgsql;
+
 
 CREATE OR REPLACE FUNCTION update_flightlines (
   flightlines_id_in UUID,
@@ -89,6 +134,29 @@ RETURNS TRIGGER AS $$
 BEGIN
   PERFORM insert_flightlines(
     flightlines_id := NEW.flightlines_id,
+    start_time := NEW.start_time,
+    end_time := NEW.end_time,
+    media_files := NEW.media_files,
+    line_notes := NEW.line_notes,
+    line_number := NEW.line_number,
+    line_index := NEW.line_index,
+
+    source_name := NEW.source_name
+  );
+  RETURN NEW;
+
+EXCEPTION WHEN raise_exception THEN
+  RAISE;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION insert_api_flightlines_from_trig()
+RETURNS TRIGGER AS $$
+BEGIN
+  PERFORM insert_api_flightlines(
+    flightlines_id := NEW.flightlines_id,
+    session_id := NEW.session_id,
     start_time := NEW.start_time,
     end_time := NEW.end_time,
     media_files := NEW.media_files,
@@ -160,6 +228,11 @@ CREATE TRIGGER flightlines_insert_trig
   INSTEAD OF INSERT ON
   flightlines_view FOR EACH ROW
   EXECUTE PROCEDURE insert_flightlines_from_trig();
+
+CREATE TRIGGER flightlines_insert_api_trig
+  INSTEAD OF INSERT ON
+  flightlines FOR EACH ROW
+  EXECUTE PROCEDURE insert_flightlines_api_from_trig();
 
 CREATE TRIGGER flightlines_update_trig
   INSTEAD OF UPDATE ON
